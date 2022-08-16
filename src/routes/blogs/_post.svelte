6259abbd6ea8b98d1484ee1e2afdc8f6bf3c4ffa
CPea <vienpro2506@gmail.com>
2022-08-17 03:00:05 +0700

    feat: center content of post

--- src/routes/blogs/_post.svelte
+++ src/routes/blogs/_post.svelte
@@ -5,7 +5,9 @@
     export let date;
 </script>
 
-<article class="px-[4vw] py-[2vw] prose prose-one-monokai">
+<article
+    class="max-w-screen-md mx-4 sm:mx-auto my-2 sm:my-6 prose prose-one-monokai"
+>
     <h1 class="mb-2 text-[#e5c07b]">{title}</h1>
     <p class="text-[#98c379] mb-4 pb-2 post-card-border">Created: {date}</p>
     <slot />
